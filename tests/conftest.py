afbcb301a5552044462492298af7ed717b9adbf2
Mirek Simek <miroslav.simek@gmail.com>
2023-03-05 10:43:48 +0100

    working on ui plugin for model builder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,21 @@
 import pytest
-from invenio_app.factory import create_api as _create_api
+from invenio_app.factory import create_app as _create_app
+
 
 @pytest.fixture(scope="module")
 def extra_entry_points():
     """Extra entry points to load the mock_module features."""
-    return {
-        "oarepo_model_builder_ui.components": [
-            "row = mock_module.components:RowComponent",
-            "list = mock_module.components:ListComponent",
-            "column = mock_module.components:ColumnComponent"
-        ],
-    }
+    return {}
+
+
+@pytest.fixture(scope="module")
+def app_config(app_config):
+    app_config["I18N_LANGUAGES"] = [("en", "English"), ("cs", "Czech")]
+    app_config["BABEL_DEFAULT_LOCALE"] = "en"
+    return app_config
+
 
 @pytest.fixture(scope="module")
 def create_app(instance_path, entry_points):
     """Application factory fixture."""
-    return _create_api
+    return _create_app
