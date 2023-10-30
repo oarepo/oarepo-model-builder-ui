025613721dfad496ca4cd8a84b5e5d2d9e41a4c1
Mirek Simek <miroslav.simek@gmail.com>
2023-10-30 20:23:46 +0100

    Unified tests, preparing upgrade to oarepo 12

--- run-tests.sh
+++ run-tests.sh
@@ -2,35 +2,43 @@
 
 set -e
 
-if [ -d .venv-builder ]; then
-  rm -rf .venv-builder
+OAREPO_VERSION=${OAREPO_VERSION:-11}
+OAREPO_VERSION_MAX=$((OAREPO_VERSION+1))
+
+BUILDER_VENV=".venv-builder"
+if test -d $BUILDER_VENV ; then
+	rm -rf $BUILDER_VENV
 fi
 
-python3 -m venv .venv-builder
-.venv-builder/bin/pip install -U setuptools pip wheel
+python3 -m venv $BUILDER_VENV
+. $BUILDER_VENV/bin/activate
+pip install -U setuptools pip wheel
+pip install -e .
 
-.venv-builder/bin/pip install -e .
 
 if [ -d mock-record ]; then
     rm -rf mock-record
 fi
 
-.venv-builder/bin/oarepo-merge tests/merged_messages.po mock-record/mock_record/translations/cs/LC_MESSAGES/messages.po
-.venv-builder/bin/oarepo-compile-model tests/model.json --output-directory mock-record -vvv
+$BUILDER_VENV/bin/oarepo-merge tests/merged_messages.po mock-record/mock_record/translations/cs/LC_MESSAGES/messages.po
+$BUILDER_VENV/bin/oarepo-compile-model tests/model.json --output-directory mock-record -vvv
 
 # check that the messages were merged correctly
 grep -q 'blah' mock-record/mock_record/translations/cs/LC_MESSAGES/messages.po
 grep -q -v 'empty' mock-record/mock_record/translations/cs/LC_MESSAGES/messages.po
 
-if [ -d .venv ]; then
-    rm -rf .venv
+VENV_TESTS=".venv-tests"
+
+if test -d $VENV_TESTS ; then
+	rm -rf $VENV_TESTS
 fi
 
-python3 -m venv .venv
-.venv/bin/pip install -U setuptools pip wheel
+python3 -m venv $VENV_TESTS
+. $VENV_TESTS/bin/activate
+pip install -U setuptools pip wheel
+pip install "oarepo>=$OAREPO_VERSION,<$OAREPO_VERSION_MAX"
+pip install pytest-invenio
 
-source .venv/bin/activate
 pip install -e 'mock-record'
-pip install pytest-invenio invenio-app 'invenio-search[opensearch2]' flask-babelex oarepo-model-builder polib inflect
 
 pytest tests
