8d1e02da6826cffa4f8d2ef7f08cf1aec8ebce62
Mirek Simek <miroslav.simek@gmail.com>
2023-12-18 16:46:47 +0100

    Clearing babel context between tests

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,7 +1,19 @@
 from oarepo_runtime.i18n import lazy_gettext, get_locale
+from flask import g
+
+
+def clear_babel_context():
+    # for invenio 12
+    try:
+        from flask_babel import SimpleNamespace
+    except ImportError:
+        return
+    g._flask_babel = SimpleNamespace()
+
 
 
 def test_translations_registered_cs(app):
+    clear_babel_context()
     with app.test_request_context(headers=[("Accept-Language", "cs")]):
         print(f"{get_locale()=}")
         print("text: ", lazy_gettext("prov"))
@@ -9,6 +21,7 @@ def test_translations_registered_cs(app):
 
 
 def test_translations_registered_en(app):
+    clear_babel_context()
     with app.test_request_context(headers=[("Accept-Language", "en")]):
         print(f"{get_locale()=}")
         print("text: ", lazy_gettext("lowest_price.hint"))
