9a4268668adf4bda0eb7ee8d5860bd3736c00e0b
Mirek Simek <miroslav.simek@gmail.com>
2023-12-18 16:39:20 +0100

    Fixing tests

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,13 +1,15 @@
-from flask_babelex import gettext
-from flask_babelex import get_locale
+from oarepo_runtime.i18n import lazy_gettext, get_locale
 
 
-def test_translations_registered(app):
+def test_translations_registered_cs(app):
     with app.test_request_context(headers=[("Accept-Language", "cs")]):
         print(f"{get_locale()=}")
-        print("text: ", gettext("prov"))
-        assert "Seznam" in gettext("prov")
+        print("text: ", lazy_gettext("prov"))
+        assert "Seznam" in lazy_gettext("prov")
+
+
+def test_translations_registered_en(app):
     with app.test_request_context(headers=[("Accept-Language", "en")]):
         print(f"{get_locale()=}")
-        print("text: ", gettext("lowest_price.hint"))
-        assert "Enter" in gettext("lowest_price.hint")
+        print("text: ", lazy_gettext("lowest_price.hint"))
+        assert "Enter" in lazy_gettext("lowest_price.hint")
